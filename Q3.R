ff5382af4f6548f6e245da2a5d7303fb7aef65aa
bimmeric <ericgoldfien>
2017-04-21 13:31:05 -0700

    Q3 Completed

--- Q3.R
+++ Q3.R
@@ -20,22 +20,33 @@ w_mat = netrw(g1, walker.num = num_nodes, damping = 1, T = num_steps, output.wal
 visit_probability = w_mat$ave.visit.prob
 g1_degree = degree(g1)
 
-plot(g1_degree,visit_probability, main = "Visit Probability for Undirected Graph", xlab = "Degree", ylab = "Visiting Probability")
+plot(g1_degree,visit_probability, main = "Visit Probability for Undirected Graph", xlab = "Degree", ylab = "Visiting Probability", ylim = c(0,.0025))
 
 ############################### Part b ###############################
 
+g2 <-random.graph.game(n = num_nodes, p = 0.01 , directed = TRUE)
+w_mat2 = netrw(g2, walker.num = num_nodes, damping = 1, T = num_steps, output.walk.path = TRUE) # get vertex sequence of random walk
 
-num_nodes <- 1000
-num_steps <- 1000
-g1 <-random.graph.game(n = num_nodes, p = 0.01 , directred = TRUE)
-w_mat = netrw(g1, walker.num = num_nodes, damping = 1, T = num_steps, output.walk.path = TRUE) # get vertex sequence of random walk
+
+#Calculate the probability of visiting a node and it's in/out/total degree
+visit_probability2 = w_mat2$ave.visit.prob
+g2_idegree = degree(g2, mode = "in")
+g2_odegree = degree(g2, mode = "out")
+g2_totdegree = degree(g2, mode = "all")
+
+#Plot the graphs of in/out/total degree versus visiting probability
+#plot(g2_idegree,visit_probability2, main = "Visit Probability for Directed Graph", xlab = "In Degree", ylab = "Visiting Probability")
+#plot(g2_odegree,visit_probability2, main = "Visit Probability for Directed Graph", xlab = "Out Degree", ylab = "Visiting Probability")
+#plot(g2_totdegree,visit_probability2, main = "Visit Probability for Directed Graph", xlab = "Total Degree", ylab = "Visiting Probability")
+
+
+############################### Part C  ###############################
+
+w_mat = netrw(g1, walker.num = num_nodes, damping = .85, T = num_steps, output.walk.path = TRUE) # get vertex sequence of random walk
 
 visit_probability = w_mat$ave.visit.prob
-g1_degree = degree(g1,mode = "out")
+g1_degree = degree(g1)
 
-plot_data = rbind(g1_degree, visit_probability)
-plot_data = plot_data[,order(plot_data[1,])]  #order by degree
-plot(plot_data[1,], plot_data[2,], xlab = "In-Degree", ylab = "Visit Probability", main = "Relation Between In Degree and Visit Probability for Directed Network")
+plot(g1_degree,visit_probability, main = "Visit Probability for Undirected Graph with D = .85", xlab = "Degree", ylab = "Visiting Probability", ylim = c(0,.0025) )
 
 
-plot(g1_degree,visit_probability, main = "Visit Probability for Directed Graph", xlab = "Degree", ylab = "Visiting Probability")
