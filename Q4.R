8e02963c7ee559d6bb7180978a5826c8d6f49a79
ericgoldfien <ericgoldfien@gmail.com>
2017-04-21 15:07:03 -0700

    Add files via upload
    Plots page rank A and B

--- Q4.R
+++ Q4.R
@@ -16,5 +16,17 @@ num_steps <- 1000
 g1 <- erdos.renyi.game(n = num_nodes, p = 0.01 , directed = TRUE)
 w_mat = netrw(g1, walker.num = num_nodes, damping = .85, T = num_steps, output.walk.path = TRUE) # get vertex sequence of random walk
 
+page_rank = w_mat$ave.visit.prob
+
+plot(page_rank, main = "Page Rank for each node", ylab = "Page Rank", ylim = c(0,.003))
+
 
 ###################################### Part B ##############################################
+
+
+w_mat2 = netrw(g1, walker.num = num_nodes, damping = .85, T = num_steps, output.walk.path = TRUE, teleport.prob = page_rank) # get vertex sequence of random walk
+
+page_rank2 = w_mat2$ave.visit.prob
+
+
+plot(page_rank2, main = "Page Rank for each node", ylab = "Page Rank", ylim = c(0,.003))
